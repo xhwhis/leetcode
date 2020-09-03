cc75f606675d8997ad8d3e7ad43aed392684a944
lws597 <lwscode@163.com>
2020-09-03 16:06:28 +0800

    2020/09/03

---
+++ 200.岛屿数量.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=cpp
+ *
+ * [200] 岛屿数量
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int dir[4][2] = {0, 1, 1, 0, 0, -1, -1, 0};
+        int n = grid.size();
+        if (n == 0) return 0;
+        int m = grid[0].size();
+        vector<vector<bool>> visited = vector<vector<bool>>(n, vector<bool>(m, false));
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    visited[i][j] = true;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    while (!q.empty()) {
+                        int x = q.front().first, y = q.front().second;
+                        q.pop();
+                        for (int k = 0; k < 4; k++) {
+                            int _x = x + dir[k][0], _y = y + dir[k][1];
+                            if ((_x >= 0 && _x < n && _y >= 0 && _y < m) && !visited[_x][_y] && grid[_x][_y] == '1') {
+                                q.push({_x, _y});
+                                visited[_x][_y] = true;
+                            }
+                        }
+                    }
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
