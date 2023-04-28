79b7315ee236f6e12ff6252d511545985742e4fa
xhwhis <hi@whis.me>
2023-04-28 18:00:58 +0800

    2023/04/28

---
+++ 1702.修改后的最大二进制字符串.rs
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode.cn id=1702 lang=rust
+ *
+ * [1702] 修改后的最大二进制字符串
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn maximum_binary_string(binary: String) -> String {}
+}
+// @lc code=end
